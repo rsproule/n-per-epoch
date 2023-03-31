d17af27caa77063f85a659aa35765a5e3a3a7e9b
Ryan Sproule <rsproule23@gmail.com>
2023-03-31 11:19:29 -0700

    update example poke with relative pathing

--- examples/rate-limited-poke/test/Poker.t.sol
+++ examples/rate-limited-poke/test/Poker.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
@@ -9,6 +9,10 @@ import { NPerEpoch } from "n-per-epoch/NPerEpoch.sol";
 contract PokeTest is Test, InteractsWithWorldID {
     Poke public poker;
 
+    function pathPrefix() public pure override returns (string memory) {
+        return "../../";
+    }
+
     function setUp() public {
         setUpWorldID();
         poker = new Poke(worldID);
@@ -18,15 +22,17 @@ contract PokeTest is Test, InteractsWithWorldID {
     function testPoke() public {
         registerIdentity();
         NPerEpoch.Settings memory settings = poker.settings();
-        NPerEpoch.RateLimitKey memory rateLimitKey = _getRateLimitKey(1, settings.epochLength);
+        NPerEpoch.RateLimitKey memory rateLimitKey = _getRateLimitKey(0, settings.epochLength);
         (uint256 nullifierHash, uint256[8] memory proof) = getProof(rateLimitKey, "poke");
         poker.poke(
             getRoot(),
+            "poke",
             nullifierHash,
             proof,
             rateLimitKey
         );
     }
+
     function _getRateLimitKey(uint256 index, uint256 epochLength)
             internal view returns (NPerEpoch.RateLimitKey memory) {
         uint256 currentEpoch = block.timestamp / epochLength;
